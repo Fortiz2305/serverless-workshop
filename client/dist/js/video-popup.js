c08ae58f04b5d8ac48ae1b75b249ea5bc0f74f3b
Francisco Ortiz <fortizabril@gmail.com>
2018-01-20 13:51:04 +0100

    Add client code

---
+++ client/dist/js/video-popup.js
@@ -0,0 +1,11 @@
+/*
+   VIDEO POP-UP
+   ========================================================================== */
+    $('.video-popup').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+    });
